cbdbba18b832c1e01be737b4640a1f9f16752699
Jubayer Himel <64546040+jrhimel@users.noreply.github.com>
2020-10-01 23:27:44 +0600

    RR BringUp
    Signed-off-by: Omansh Krishn <omansh@duck.com>

--- rr_santoni.mk
+++ rr_santoni.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019 The Lineage-OS Project
+# Copyright (C) 2019 The Resurrection Remix-OS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,23 +16,31 @@
 
 $(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
 
-# Inherit some common Lineage OS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Resurrection Remix-OS stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := santoni
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4X
-PRODUCT_NAME := lineage_santoni
+PRODUCT_NAME := rr_santoni
 BOARD_VENDOR := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 
-# Boot animation
-TARGET_BOOT_ANIMATION_RES := 720
+# RR Wallpapers
+BUILD_RR_WALLPAPERS := true
 
+# RR Stuffs
+TARGET_FACE_UNLOCK_SUPPORTED := true
+export RR_BUILDTYPE := Official
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+	
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="santoni-user 7.1.2 N2G47H V9.5.10.0.NAMMIFD release-keys"
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/santoni/santoni:7.1.2/N2G47H/V9.5.10.0.NAMMIFD:user/release-keys"
+BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
