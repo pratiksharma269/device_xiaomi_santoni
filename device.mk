07c7b07627e4b10230310a0034007c568aec59ed
Bruno Martins <bgcngm@gmail.com>
2022-10-02 23:50:32 +0200

    santoni: Drop component overrides
    This is no longer needed as of Android 13.
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    
    Signed-off-by: Omansh Krishn <omansh11597@gmail.com>

--- device.mk
+++ device.mk
@@ -188,10 +188,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
-# Component overrides
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
 # Dex
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI \
