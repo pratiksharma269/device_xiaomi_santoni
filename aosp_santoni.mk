5c92203200891c02e1fae5cc8b181c5b1786cd52
Omansh Krishn <omansh11597@gmail.com>
2023-03-25 16:09:07 +0000

    santoni: PixelExperience: added some misc pixel feature flags
    Signed-off-by: Omansh Krishn <omansh11597@gmail.com>

--- aosp_santoni.mk
+++ aosp_santoni.mk
@@ -25,6 +25,14 @@ $(call inherit-product, device/xiaomi/santoni/device.mk)
 # Inherit some common PixelExperience stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
+#MISC Flags
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_BLUR := true
+USE_PIXEL_CHARGING := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
