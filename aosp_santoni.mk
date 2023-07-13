b46367a1a74d9c4e7811149208781af013a7a964
Omansh Krishn <omansh11597@gmail.com>
2023-07-13 16:03:15 +0000

    santoni: PixelExtended: added some PixelExtended flags
    Signed-off-by: Omansh Krishn <omansh11597@gmail.com>

--- aosp_santoni.mk
+++ aosp_santoni.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2023 The PixelExperience Project
+# Copyright (C) 2023 The PixelExtended Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 # Inherit from santoni device
 $(call inherit-product, device/xiaomi/santoni/device.mk)
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common PixelEx(perience)/(tended) stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 #MISC Flags
@@ -30,9 +30,13 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_USES_BLUR := true
 USE_PIXEL_CHARGING := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+PEX_MAINTAINER := Omansh Krishn
+TARGET_GAPPS_ARCH := arm64
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
