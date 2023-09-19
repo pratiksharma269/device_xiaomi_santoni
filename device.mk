724292b2acf6fbc84137d1886db185eb852ec409
Michael Bestas <mkbestas@lineageos.org>
2023-09-19 14:11:01 +0000

    santoni: Build Lineage Health HAL
    Signed-off-by: Omansh Krishn <omansh11597@gmail.com>

--- device.mk
+++ device.mk
@@ -331,6 +331,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.santoni
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
