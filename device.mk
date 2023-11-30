d9c20ff3e20622ff2d4cec1a040bfa4e0f6c110c
balgxmr <jose@pixelos.net>
2023-11-30 23:06:45 +0530

    santoni: Build android.frameworks.sensorservice F linker  : CANNOT LINK EXECUTABLE /vendor/bin/mm-pp-dpps: library android.frameworks.sensorservice@1.0.so not found: needed by main executable
    Signed-off-by: Omansh Krishn <omansh11597@gmail.com>

--- device.mk
+++ device.mk
@@ -467,6 +467,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
