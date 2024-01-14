e8303e5be30fc757f9805f6e722787f30631fc0e
Alone0316 <rutujamandlik06@gmail.com>
2024-01-14 17:43:32 +0530

    santoni: Build libstdc++.so
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/gx_fpd": library "libstdc++.so" not found: needed by main executable
    
    Signed-off-by: Omansh Krishn <omansh11597@gmail.com>

--- device.mk
+++ device.mk
@@ -186,6 +186,8 @@ PRODUCT_PACKAGES += \
     libstdc++_vendor \
     libgui_vendor
 
+PRODUCT_PACKAGES += libstdc++
+
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
