733ee066631f35892f2ba99b69ac5c885e13ec74
Omansh Krishn <omansh@duck.com>
2024-03-16 19:39:24 +0530

    santoni: Address mke2fs command failure due to invalid filesystem options
    mke2fs version used by aosp for a11 is 1.44.4 (18-Aug-2018)
    On distros with newer version, the default /etc/mke2fs.conf contains
    some flags which are not supported by the older version.
    
    The compilation fails saying: Invalid filesystem option set
    
    Signed-off-by: Omansh Krishn <omansh11597@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,3 +20,19 @@ while true; do
     curl -L -o "$GCAM_FILE" "$GCAM_DOWNLOAD_URL" || { echo "GCam/Camera.apk Download failed. Retrying..."; continue; }
 done
 croot
+
+if grep -q 'metadata_csum_seed' /etc/mke2fs.conf && filecontent=$(cat /etc/mke2fs.conf | sed 's/metadata_csum_seed,//'); then
+    if ! echo "$filecontent" > /etc/mke2fs.conf; then
+        echo '/etc/mke2fs.conf contains invalid filesystem option metadata_csum_seed'
+        echo 'Aborting the build.'
+        exit 1
+    fi
+fi
+
+if grep -q 'orphan_file' /etc/mke2fs.conf && filecontent=$(cat /etc/mke2fs.conf | sed 's/,orphan_file//'); then
+    if ! echo "$filecontent" > /etc/mke2fs.conf; then
+        echo '/etc/mke2fs.conf contains invalid filesystem option orphan_file'
+        echo 'Aborting the build.'
+        exit 1
+    fi
+fi
