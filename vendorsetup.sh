63c1a5859dbdea6b55ed10fef2c1f7f8d6173d9c
Omansh Krishn <omansh11597@gmail.com>
2024-08-12 18:13:06 +0530

    vendorsetup: Allow installing fake [odm|product|system_ext] packages
    https://github.com/LeafOS-experimental/android_build/commit/a33bf0afb3d076dccb000817344c400f2b1d5c52.patch
    
    Signed-off-by: Omansh Krishn <omansh11597@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,16 @@
 croot; cd bionic
-
+# libstdc++: Rename vendor variant to libstdc++_vendor
 PATCH=$(curl -s https://github.com/LeafOS-experimental/android_bionic/commit/2381db9839fa4abeae48ade11ee46f0a53eb4c9c.patch)
 if ! echo "$PATCH" | git apply --reverse --check 2>/dev/null; then
         echo "$PATCH" | git apply
 fi
+unset PATCH
 
+croot; cd build/make
+# Allow installing fake [odm|product|system_ext] packages
+PATCH=$(curl -s https://github.com/LeafOS-experimental/android_build/commit/a33bf0afb3d076dccb000817344c400f2b1d5c52.patch)
+if ! echo "$PATCH" | git apply --reverse --check 2>/dev/null; then
+        echo "$PATCH" | git apply
+fi
+unset PATCH
 croot
